8afb1f478b05d0c46158558c499cd46b945bee45
mukulraw199517@gmail.com <Zedistark1@>
2017-08-31 19:52:35 +0530

    asdasd

---
+++ app/src/main/java/com/vedictouch/vedictouch/Category.java
@@ -0,0 +1,99 @@
+package com.vedictouch.vedictouch;
+
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+public class Category extends AppCompatActivity {
+
+    ImageButton back;
+    RelativeLayout single , natal , corporate , beauty , couple , offer;
+
+    CoordinatorLayout categoryLayout;
+
+    BottomSheetBehavior bottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        back = (ImageButton)findViewById(R.id.back);
+
+        categoryLayout = (CoordinatorLayout)findViewById(R.id.category_layout);
+
+        single = (RelativeLayout)findViewById(R.id.single);
+        natal = (RelativeLayout)findViewById(R.id.natal);
+        corporate = (RelativeLayout)findViewById(R.id.chair);
+        beauty = (RelativeLayout)findViewById(R.id.beauty);
+        couple = (RelativeLayout)findViewById(R.id.couple);
+        offer = (RelativeLayout)findViewById(R.id.offer);
+
+
+
+        View bottom = findViewById(R.id.book);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom);
+
+
+        single.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+
+        natal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+
+        corporate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+
+        beauty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+
+        couple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+
+    }
+}
--- app/src/main/java/com/vedictouch/vedictouch/MainActivity.java
+++ app/src/main/java/com/vedictouch/vedictouch/MainActivity.java
@@ -1,26 +1,27 @@
 package com.vedictouch.vedictouch;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    RelativeLayout relative;
     Button book;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        relative = (RelativeLayout)findViewById(R.id.relative);
         book = (Button)findViewById(R.id.book);
-        relative.setClipToOutline(true);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction ft = fragmentManager.beginTransaction();
@@ -31,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
 
 
+
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this , Category.class);
+                startActivity(intent);
+
+            }
+        });
 
 
 
